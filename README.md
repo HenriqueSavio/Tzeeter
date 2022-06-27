a043360bda7428a7ba981565d1b650428ec73c65
HenriqueSavio <henriquesaviots@gmail.com>
2022-06-26 22:24:03 -0300

    Lista 5

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 
 [Lista 04](https://github.com/HenriqueSavio/Tzeeter/tree/3f04b58e7c7240087824bb8f994239cf586e8cf2)
 
+[Lista 05]()
+
--- tzeeter.js
+++ tzeeter.js
@@ -71,38 +71,22 @@ for (const textarea of textAreas) {
     blabla(textarea);    
 }
 
-function blabla(textareapai){
-
-    
+function blabla(textareapai){    
     const textArea = textareapai.querySelector('textarea');
     const butao = textareapai.querySelector("#butao");
     const contador = textareapai.querySelector("#contador");
-
     textArea.addEventListener('input', (event) => {
-
-
-            
         textArea.cols = textArea.scrollWidth;  
         textArea.style.height = 'auto';
         textArea.style.height = (textArea.scrollHeight) + "px";
-
-
-
-
-
         let tamanhoText = 140 - textArea.value.length;
         console.log(tamanhoText);
-
         contador.classList.remove('contadorExtrapola');
         contador.classList.remove('contadorPerigo');
-
-
-
         if(tamanhoText < 0){
             contador.classList.add('contadorExtrapola');
             butao.disabled = true;
             contador.innerHTML = tamanhoText;
-
         }else if(tamanhoText < 40){
             contador.classList.add('contadorPerigo');
             butao.disabled = false;
@@ -111,16 +95,59 @@ function blabla(textareapai){
             butao.disabled = false;
             contador.innerHTML = "";
         }
-
         if(tamanhoText == 140){
             butao.disabled = true;
         }
-
-
     })
 
 }
 
-function inputTextIdle(event){
+
+/*
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  */
+
+    var entrar = document.getElementById('entrar');
+
+    document.addEventListener("submit", function(event){
+        var email = document.getElementById('validationEmail');
+        var senha = document.getElementById('validationSenha');
     
-}
+        if(email.includes("@") && email.includes(".com") && senha.ariaValueMax.length > 5){
+            entrar.disabled = false;
+        }
+    });
+
+    document.addEventListener("submit", function(event){
+        const inscreve = document.getElementById('inscrever');
+        const nome = document.getElementById('validationCustomUsername');
+        const email = document.getElementById('validationCustom01');
+        const senha1 = document.getElementById('validationCustom02');
+        const senha2 = document.getElementById('validationCustom03');
+        const mes = document.getElementById('validationCustom04');
+        const dia = document.getElementById('validationCustom05');
+        const ano = document.getElementById('validationCustom06');
+        const termos = document.getElementById('termos');
+        
+        if(termos.checked){
+            inscreve.disabled = false;
+        }
+    })
